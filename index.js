da8725b8c551f064ff43da904f78c93195217ee8
mandareis <amanda@depaula.xyz>
2020-11-16 08:08:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(array, events) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${events} gift!`);
+  }
+  return newArray;
+}
+
+// function writeCards(array, events) {
+//   return array.map(
+//     (name) => `Thank you, ${name}, for the wonderful ${events} gift!`
+//   );
+// }
+// only pass positive integers here
+function countDown(integer) {
+  let i = integer;
+  while (i >= 0) {
+    console.log(i--);
+  }
+}
+countDown(10);
